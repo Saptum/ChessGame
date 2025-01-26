ae45f4f2f91e235f50cd189db636ce12ed3943ac
Thanatos <nikazavgorodniaya@gmail.com>
2025-01-26 13:08:40 +0200

    feat: Add Board class and integrate into main for console display

--- ChessGame/board.cpp
+++ ChessGame/board.cpp
@@ -1,16 +1,47 @@
-#include "board.h"
-#include <iostream>
+﻿#include "board.h"
 
-Board::Board() {
-	squares.resize(8, std::vector<Piece*>(8, nullptr));
+Board::Board()
+{
+	// Инициализация доски: создание 8 строк по 8 столбцов
+	squares = std::vector<std::vector<char>>(8, std::vector<char>(8, ' '));
+
+
+	// Расстановка фигур на доске
+	const std::string initialSetup[8] = {
+        "rnbqkbnr",
+        "pppppppp",
+        "        ",
+        "        ",
+        "        ",
+        "        ",
+        "PPPPPPPP",
+        "RNBQKBNR"
+	};
+
+    for (int row=0;row <8;++row)
+    {
+	    for (int col =0;col < 8;++col)
+	    {
+            squares[row][col] = initialSetup[row][col];
+	    }
+    }
 }
 
-void Board::Initialize() {
-	//squares[0][0] = new Rook(Color::WHITE);
+void Board::Display() const
+{
+    std::cout << " a b c d e f g h\n";// Метки столбцов
+    for (int row=0;row<8;++row)
+    {
+        std::cout << 8 - row << ' ';// Метки строк
+        for (int col =0;col <8 ;++col)
+        {
+            std::cout << squares[row][col] << ' ';
+        }
+        std::cout << 8 - row << '\n';
+    }
+    std::cout << " a b c d e f g h\n";// Метки столбцов
 }
 
-void Board::Display() const{
-	for (int row = 0; row < 8; ++row){
 
-	}
-}
+
+
--- ChessGame/board.h
+++ ChessGame/board.h
@@ -1,17 +1,21 @@
-#pragma once
+﻿#pragma once
 #ifndef BOARD_H
 #define BOARD_H
+
+#include <iostream>
 #include <vector>
 #include "piece.h"
+//using namespace std;
+
 
 class Board {
 public:
-	Board();
-	void Initialize();
-	void Display() const;
+	Board();// Конструктор класса
+	void Display() const;// Метод для отображения доски
+
 
 private:
-	std::vector<std::vector<Piece*>> squares;
+	std::vector<std::vector<char>> squares;// Двумерный вектор для хранения состояния доски
 
 };
 #endif
