0753d08d81e614f70d461989faf70c5fd48b81ac
Thanatos <nikazavgorodniaya@gmail.com>
2025-01-27 22:09:18 +0200

    update

--- ChessGame/board.h
+++ ChessGame/board.h
@@ -12,7 +12,7 @@ class Board {
 public:
 	Board();// Конструктор класса
 	void Display() const;// Метод для отображения доски
-
+	bool MovePiece(int startX, int startY, int endX, int endY);
 
 private:
 	std::vector<std::vector<char>> squares;// Двумерный вектор для хранения состояния доски
--- ChessGame/piece.h
+++ ChessGame/piece.h
@@ -1,1 +1,22 @@
 #pragma once
+
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+enum class Color { White, Black  };
+
+class Piece
+{
+public:
+
+	Piece(Color color) : color(color) {}
+	virtual ~Piece() = default;
+	Color GetColor() const { return color; }
+	virtual char GetSymbol() const = 0;
+	virtual bool IsValidMove(int startX, int startY, int endX, int endY) const = 0;
+
+protected:
+	Color color;
+};
