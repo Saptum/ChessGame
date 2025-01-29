e8d08763a9bc7f7f3b2c26c311de0d93a2535770
Thanatos <nikazavgorodniaya@gmail.com>
2025-01-29 14:05:19 +0200

    add  bool Board::MovePiece

--- ChessGame/board.cpp
+++ ChessGame/board.cpp
@@ -40,37 +40,57 @@ Board::Board()
 
 void Board::Display() const
 {
-    const char blackSquare = ' # ';// Символ для черной клетки
-    const char whiteSquare = ' '; // Символ для белой клетки
+    //const char blackSquare = ' # ';// Символ для черной клетки
+    //const char whiteSquare = ' '; // Символ для белой клетки
 
 
-    std::cout << " a b c d e f g h\n";// Метки столбцов
-    std::cout << "+-----------------+\n"; // Верхняя рамка
+    std::cout << "  a b c d e f g h\n";// Метки столбцов
+    std::cout << " +-----------------+\n"; // Верхняя рамка
     for (int row=0;row<8;++row)
     {
         std::cout << 8 - row << " | ";// Метки строк и левая рамка
         for (int col =0;col <8 ;++col)
         { 
-            // Определение цвета клетки
-            bool isBlack = (row + col) % 2 == 0;
-            if (squares[row][col] != ' ')
+            if (squares[row][col])
             {
                 // Если на клетке есть фигура, отображаем ее
-                std::cout << squares[row][col] << ' ';
+                std::cout << squares[row][col]->GetSymbol() << ' ';
             }
             else
             {
-                // Иначе отображаем цвет клетки
-                std::cout << (isBlack ? blackSquare : whiteSquare) << ' ';
+                bool isBlack = (row + col) % 2 == 0;
+                std::cout << (isBlack ? '#' : ' ') << ' ';
             }
-            //std::cout << squares[row][col] << ' ';
+           
         }
         std::cout<< " | " << 8 - row << '\n';// Правая рамка и метка строки
     }
     std::cout << " +-----------------+\n"; // Нижняя рамка
-    std::cout << " a b c d e f g h\n";// Метки столбцов
+    std::cout << "  a b c d e f g h\n";// Метки столбцов
 }
 
+
+bool Board::MovePiece(int startX, int startY, int endX, int endY)
+{
+	if (startX < 0 || startX >= 8 || startY < 0 || startY >= 8 || endX < 0 || endX >= 8 || endY < 0 || endY >= 8)
+	{
+        return false;// Координаты вне диапазона
+	}
+
+    auto& piece = squares[startY][startX];
+    if (!piece) {
+        return false; // Нет фигуры в начальной позиции
+    }
+
+	if (piece->IsValidMove(startX,startY,endX,endY))
+	{
+        squares[endY][endX] = std::move(piece);
+        squares[startY][startX] = nullptr;
+        return true;
+	}
+
+    return false;// Недопустимый ход
+}
 
 
 
