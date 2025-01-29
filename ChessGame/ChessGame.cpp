d0dd5a8722a8eb59a6e62a64301416afa9dd1900
Thanatos <nikazavgorodniaya@gmail.com>
2025-01-29 14:12:53 +0200

    update classchessGame

--- ChessGame/ChessGame.cpp
+++ ChessGame/ChessGame.cpp
@@ -1,12 +1,37 @@
 ﻿
 
-#include <iostream>
+
 #include "board.h"
 
 int main()
 {
     Board chessBoard;// Создание объекта шахматной доски
-    chessBoard.Display();// Отображение доски
+    std::string move;
+    while (true)
+    {
+	    chessBoard.Display();// Отображение доски
+        std::cout << "Введите ход (например, e2e4) или 'exit' для выхода: ";
+        std::cin >> move;
+	    if (move == "exit")
+	    {
+		    break;
+	    }
+	    if (move.length() == 4)
+	    {
+            int startX = move[0] - 'a';
+            int startY = 8 - (move[1] - '0');
+            int endX = move[2] - 'a';
+            int endY = 8 - (move[3] - '0');
+            if (!chessBoard.MovePiece(startX, startY, endX, endY)) {
+                std::cout << "Недопустимый ход. Попробуйте снова.\n";
+            }
+	    }
+	    else
+	    {
+            std::cout << "Неверный формат ввода. Попробуйте снова.\n";
+	    }
+    }
+    
     return 0;
 }
 
